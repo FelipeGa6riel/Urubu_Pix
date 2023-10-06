3a03cad18979e48f37c33454c92cfea1a110fe81
Felipe <felipegabriel.play@gmail.com>
2023-10-06 16:08:34 -0300

    create user entity

---
+++ src/main/java/com/urubu/pix/domain/user/User.java
@@ -0,0 +1,28 @@
+package com.urubu.pix.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String cpf;
+    private BigDecimal balance;
+
+}
---
+++ src/main/java/com/urubu/pix/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.urubu.pix.repositories;
+
+import com.urubu.pix.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByCpf(String cpf);
+
+   Optional<User> findUserById(Long id);
+
+}
