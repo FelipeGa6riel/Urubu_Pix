985a18798a84e5e23c38ecb7ba28b705256292da
Felipe <felipegabriel.play@gmail.com>
2023-10-06 15:17:25 -0300

    initial commit

---
+++ src/main/java/com/urubu/pix/UrubuPixApplication.java
@@ -0,0 +1,13 @@
+package com.urubu.pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrubuPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrubuPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urubu/pix/UrubuPixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urubu.pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrubuPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
