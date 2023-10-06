69aaeb8f9ec68026b1968eb95428d058aeaccc68
Felipe <felipegabriel.play@gmail.com>
2023-10-06 16:12:14 -0300

    create transaction entity

---
+++ src/main/java/com/urubu/pix/controller/TransactionController.java
@@ -0,0 +1,11 @@
+package com.urubu.pix.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+
+}
---
+++ src/main/java/com/urubu/pix/domain/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package com.urubu.pix.domain.transaction;
+
+import com.urubu.pix.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDate timeStamp;
+
+}
