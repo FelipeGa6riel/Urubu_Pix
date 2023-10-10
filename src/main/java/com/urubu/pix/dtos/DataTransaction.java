fec2e1088b6abdec31a79bbe14c0e3e15e85ea7e
Felipe <felipegabriel.play@gmail.com>
2023-10-10 15:11:49 -0300

    fix: correct dto

--- src/main/java/com/urubu/pix/dtos/DataTransaction.java
+++ src/main/java/com/urubu/pix/dtos/DataTransaction.java
@@ -1,5 +1,6 @@
 package com.urubu.pix.dtos;
 
+import com.urubu.pix.domain.transaction.Transaction;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,7 +17,11 @@ public record DataTransaction(
         public DataTransaction {
 
         }
-        public DataTransaction(Long senderId, BigDecimal value, LocalDateTime data) {
-                this(value, senderId, 0L, data);
+        public DataTransaction(Long senderId, BigDecimal value, Long receiverId,LocalDateTime data) {
+                this(value, senderId, receiverId, data);
+        }
+
+        public  DataTransaction(Transaction transaction) {
+                this(transaction.getAmount(),transaction.getSender().getId(),transaction.getReceiver().getId(),transaction.getData());
         }
 }
