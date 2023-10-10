f0f5f281974f80743f1fc65c2544bd25aef3f0c6
Felipe <felipegabriel.play@gmail.com>
2023-10-09 22:54:24 -0300

    Updating transactions and creating custom query for persistence

--- src/main/java/com/urubu/pix/controller/TransactionController.java
+++ src/main/java/com/urubu/pix/controller/TransactionController.java
@@ -1,11 +1,44 @@
 package com.urubu.pix.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.urubu.pix.domain.transaction.Transaction;
+import com.urubu.pix.dtos.DataTransaction;
+import com.urubu.pix.repositories.TransactionRepository;
+import com.urubu.pix.services.TransactionService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
 
+    @Autowired
+    TransactionService transactionService;
 
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Transaction> createTransaction(@RequestBody DataTransaction dataTransaction) {
+        var transaction = transactionService.createTransaction(dataTransaction);
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Transaction>> listTransactionId(@PathVariable Long id) {
+        var transaction = transactionRepository.findById(id);
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DataTransaction>> listTransactioByUserId(@RequestBody DataTransaction dataTransaction) {
+        var transaction = transactionRepository.findTransactioByIdUser(dataTransaction.senderId());
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
 }
