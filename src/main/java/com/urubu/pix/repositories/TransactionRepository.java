6908e4d76e3840f3a47e8204c1f2fa6fd805cc1e
Felipe <felipegabriel.play@gmail.com>
2023-10-10 15:15:21 -0300

    feat: return pageable in find personalized

--- src/main/java/com/urubu/pix/repositories/TransactionRepository.java
+++ src/main/java/com/urubu/pix/repositories/TransactionRepository.java
@@ -2,13 +2,13 @@ package com.urubu.pix.repositories;
 
 import com.urubu.pix.domain.transaction.Transaction;
 import com.urubu.pix.dtos.DataTransaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    @Query("SELECT new com.urubu.pix.dtos.DataTransaction(t.sender.id, t.amount, t.data) FROM transactions t WHERE t.sender.id = :id")
-    List<DataTransaction> findTransactioByIdUser(Long id);
+    @Query("SELECT new com.urubu.pix.dtos.DataTransaction(t.sender.id, t.amount,t.receiver.id, t.data) FROM transactions t WHERE t.sender.id = :id")
+    Page<DataTransaction> findTransactioByUserId(Long id, Pageable page);
 }
