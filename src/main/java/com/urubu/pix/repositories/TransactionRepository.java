7cd41a6cdab0a5ee2021737516f3a9abcb9a6759
Felipe <felipegabriel.play@gmail.com>
2023-10-13 19:20:12 -0300

    feat: find transaction by recent date

--- src/main/java/com/urubu/pix/repositories/TransactionRepository.java
+++ src/main/java/com/urubu/pix/repositories/TransactionRepository.java
@@ -1,7 +1,6 @@
 package com.urubu.pix.repositories;
 
 import com.urubu.pix.domain.transaction.Transaction;
-import com.urubu.pix.dtos.DataDeposit;
 import com.urubu.pix.dtos.DataTransaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,7 +13,9 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     @Query("SELECT new com.urubu.pix.dtos.DataTransaction(t.sender.id, t.amount,t.typeTransaction,t.receiver.id,t.timeStamp) FROM Transactions t WHERE t.sender.id = :id")
-    Page<DataTransaction> findTransactioByUserId(Long id, Pageable page);
+    Page<DataTransaction> findTransactionsByUserId(Long id, Pageable page);
 
+    @Query("SELECT t FROM Transactions t WHERE t.sender.id = :id ORDER BY t.timeStamp DESC LIMIT 1")
+    Transaction findTransactionByDateRecent(Long id);
     List<Transaction> findTransactionsByTimeStamp(LocalDateTime data);
 }
